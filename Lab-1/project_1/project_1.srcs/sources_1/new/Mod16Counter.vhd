353c3c6e187bc269253e586ad39c34b0a6570148
Justin Igmen <55956362+igmen-j@users.noreply.github.com>
2021-01-18 09:21:09 -0600

    Lab 1 Initial Upload

---
+++ Lab-1/project_1/project_1.srcs/sources_1/new/Mod16Counter.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01/18/2021 09:15:16 AM
+-- Design Name: 
+-- Module Name: Mod16Counter - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Mod16Counter is
+    Port ( CLK : in STD_LOGIC;
+           CLR : in STD_LOGIC;
+           Q : out STD_LOGIC_VECTOR (3 downto 0));
+end Mod16Counter;
+
+architecture Behavioral of Mod16Counter is
+
+begin
+
+
+end Behavioral;
